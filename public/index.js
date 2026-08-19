82a71e0dd56834bea3f55279a2e890245c7e1f12
potaajyu1729 <potaajyu.kosen.jp@gmail.com>
2026-08-19 10:25:40 +0900

    publicの変更

--- public/index.js
+++ public/index.js
@@ -1,2 +1,5 @@
-const message = await fetch("/welcome-message")
-document.querySelector("#welcomeMessage").innerText = await message.text()
+const messagePromise = fetch("/welcome-message").then((response) => response.text())
+const minimumLoadingTime = new Promise((resolve) => setTimeout(resolve, 1500))
+
+const [message] = await Promise.all([messagePromise, minimumLoadingTime])
+document.querySelector("#welcomeMessage").innerText = message
